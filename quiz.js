bbd8eb1d1bce8199d74b837673be205501147d5a
Foxy221345 <85758375+Foxy221345@users.noreply.github.com>
2021-11-08 16:59:32 -0600

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,71 @@
+player_1_name= localStorage.getItem("player_1_name");
+player_2_name= localStorage.getItem("player_2_name");
+
+player_1_score=0;
+player_2_score=0;
+
+document.getElementById("player_1_name").innerHTML=player_1_name+":";
+document.getElementById("player_2_name").innerHTML=player_2_name+":";
+document.getElementById("player_1_score").innerHTML=player_1_score;
+document.getElementById("player_2_score").innerHTML=player_2_score;
+
+document.getElementById("player_question").innerHTML="Question Turn- "+player_1_name;
+document.getElementById("player_answer").innerHTML="Answer Turn- "+player_2_name;
+
+function send()
+{
+    number1= document.getElementById("number_1").value;
+    number2= document.getElementById("number_2").value;
+    actual_answer= parseInt(number1)*parseInt(number2);
+
+    question_number= "<h4>"+ number1 + " X "+ number2 + "</h4>";
+    input_box="<br>Answer :<input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+
+    row= question_number + input_box + check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("number_1").value="";
+    document.getElementById("number_2").value="";
+}
+question_turn="player1";
+answer_turn="player2";
+function check()
+{
+    get_answer=document.getElementById("input_check_box").value;
+    if(get_answer==actual_answer)
+    {
+        window.alert("The question was correctly answered by the player. So the player is getting an extra point.");
+
+        if(answer_turn=="player1")
+        {
+            answer_turn="player2";
+            player_1_score=player_1_score+1;
+            document.getElementById("player_1_score").innerHTML=player_1_score;
+            document.getElementById("player_answer").innerHTML="Answer Turn- "+player_2_name;
+        }
+        else
+        {
+            answer_turn="player1";
+            player_2_score= player_2_score + 1;
+            document.getElementById("player_2_score").innerHTML=player_2_score;
+            document.getElementById("player_answer").innerHTML="Answer Turn- "+player_1_name;
+        }
+    
+        if(question_turn=="player1")
+        {
+            question_turn="player2";
+            document.getElementById("player_question").innerHTML="Question Turn-"+ player_2_name;
+        }
+        else
+        {
+            question_turn="player1";
+            document.getElementById("player_question").innerHTML="Question Turn-"+ player_1_name;
+        }
+    }
+    else
+    {
+        window.alert("The question was not correctly answered by the player. So the player is not getting a point.");
+    }
+
+    document.getElementById("output").innerHTML="";
+}
